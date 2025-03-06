7d2114bb6fa8f546d6e31c951d94f481f5064282
vet-q <kwonok5945@gmail.com>
2025-03-06 16:33:57 +0900

    third

--- main.js
+++ main.js
@@ -1,20 +1,20 @@
+// main.js (루트)
+const { app, BrowserWindow } = require("electron");
+let mainWindow;
+
 function createWindow() {
-    let win = new BrowserWindow({
-        width: 800,
-        height: 600,
-        webPreferences: {
-            nodeIntegration: true
-        }
-    });
-
-    win.loadURL(
-        url.format({
-            pathname: path.join(__dirname, 'build', 'index.html'),
-            protocol: 'file:',
-            slashes: true
-        })
-    );
-
-    // ✅ 개발자 도구 열기
-    win.webContents.openDevTools();
+  mainWindow = new BrowserWindow({ width: 1200, height: 800 });
+  // React 개발 서버 주소를 로드
+  mainWindow.loadURL("http://localhost:3000");
 }
+
+app.whenReady().then(() => {
+  createWindow();
+  app.on("activate", function () {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
+
+app.on("window-all-closed", function () {
+  if (process.platform !== "darwin") app.quit();
+});
